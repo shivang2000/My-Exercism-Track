0aab2519989b7cfafe0a7f8f9d3eb7ff2dfa730a
shivang2000 <shivangchheda@gmail.com>
2026-01-05 00:23:25 +0530

    leap exercise

---
+++ typescript/leap/leap.ts
@@ -0,0 +1,14 @@
+export function isLeap(year: number) {
+  if (year % 4 === 0) {
+    if (year % 100 === 0) {
+      if (year % 400 === 0) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return true;
+    }
+  }
+  return false;
+}
